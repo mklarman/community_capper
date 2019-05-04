d92ec607ddd0bf74dd65195d0fd42ede39fa8ba1
mklarman <mklarman@hotmail.com>
2019-05-04 11:20:56 -0400

    matchup helper functions

--- app/helpers/matchups_helper.rb
+++ app/helpers/matchups_helper.rb
@@ -1,41 +1,14 @@
 module MatchupsHelper
 
-	def avm_opp(team, sport)
+	def get_all_home_team_matchups (sport, team)
 
-		@all_matchups = []
-		@home_fave_under3 = []
-		@home_fave_under7 = []
-		@home_fave_under10 = []
-		@home_fave_under14 = []
-		@home_fave_14_or_more = []
+		Matchup.all.each do |m|
 
-		@road_fave_under3 = []
-		@road_fave_under7 = []
-		@road_fave_under10 = []
-		@road_fave_under14 = []
-		@road_fave_14_or_more = []
+			if m.fav == team || m.dog == team
 
-		@road_dog_under3 = []
-		@road_dog_under7 = []
-		@road_dog_under10 = []
-		@road_dog_under14 = []
-		@road_dog_14_or_more = []
+				if m.winner.length !=0
 
-		@home_dog_under3 = []
-		@home_dog_under7 = []
-		@home_dog_under10 = []
-		@home_dog_under14 = []
-		@home_dog_14_or_more = []
-
-
-		Matchup.each do |m|
-
-			if m.sport == sport
-
-				if m.fav == team || m.dog == team
-
-					@all_matchups.push(m)
-
+					@home_team_matchups.push(m)
 
 				end
 
@@ -44,104 +17,37 @@ module MatchupsHelper
 
 		end
 
-		@all_matchups.each do |m|
 
-			if m.fav == team
+	end
 
-				if m.fav_field == "home"
+	def get_all_road_team_matchups(sports, team)
 
-					if m.spread < 3
+		Matchup.all.each do |m|
 
-						@home_fave_under3.push(m)
+			if m.fav == team || m.dog == team
 
+				if m.winner.length != 0
 
-					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
-
-						@home_fave_under7.push(m)
-					
-
-					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
-
-						@home_fave_under10.push(m)
-					
-
-					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
-
-						@home_fave_under14.push(m)
-
-
-					else
-
-						@home_fave_14_or_more.push(m)
-
-
-					end
-
-
-				else
-
-					if m.spread < 3
-
-						@road_fave_under3.push(m)
-
-
-					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
-
-						@road_fave_under7.push(m)
-					
-
-					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
-
-						@road_fave_under10.push(m)
-					
-
-					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
-
-						@road_fave_under14.push(m)
-
-
-					else
-
-						@road_fave_14_or_more.push(m)
-
-
-					end
+					@road_team_matchups.push(m)
 
 				end
 
-			elsif m.dog == team
 
-				if m.dog_field == "home"
+			end
 
-					if m.spread < 3
+		end
 
-						@home_dog_under3.push(m)
+	end
 
+	def analyze_home_matchups(team, sport)
 
-					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+		@home_team_matchups.each do |m|
 
-						@home_dog_under7.push(m)
-					
+			if m.fav == team
 
-					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+				@all_home_matchups.push(m)
 
-						@home_dog_under10.push(m)
-					
-
-					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
-
-						@home_dog_under14.push(m)
-
-
-					else
-
-						@home_dog_14_or_more.push(m)
-
-
-					end
-
-				
-				else
+				if m.fav_field == "home"
 
 					if m.spread < 3
 
@@ -171,11 +77,349 @@ module MatchupsHelper
 					end
 
 
+				else
+
+					if m.spread < 3
+
+						@home_dog_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@home_dog_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@home_dog_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@home_dog_under14.push(m)
+
+
+					else
+
+						@home_dog_14_or_more.push(m)
+
+
+					end
+
+				end
+
+			elsif m.dog == team
+
+				@all_home_matchups.push(m)
+
+				if m.dog_field == "home"
+
+					if m.spread < 3
+
+						@road_fave_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@road_fave_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@road_fave_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@road_fave_under14.push(m)
+
+
+					else
+
+						@road_fave_14_or_more.push(m)
+
+
+					end
+
+				
+				else
+
+					if m.spread < 3
+
+						@home_fave_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@home_fave_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@home_fave_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@home_fave_under14.push(m)
+
+
+					else
+
+						@home_fave_14_or_more.push(m)
+
+
+					end
+
+
 				end
 
 			end
 
 		end
+
+	end
+
+	def analyze_road_matchups(sport, team)
+
+		@road_team_matchups.each do |m|
+
+			if m.fav == team
+
+				@all_road_matchups.push(m)
+
+				if m.fav_field == "home"
+
+					if m.spread < 3
+
+						@R_road_dog_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@R_road_dog_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@R_road_dog_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@R_road_dog_under14.push(m)
+
+
+					else
+
+						@R_road_dog_14_or_more.push(m)
+
+
+					end
+
+
+				else
+
+					if m.spread < 3
+
+						@R_home_dog_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@R_home_dog_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@R_home_dog_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@R_home_dog_under14.push(m)
+
+
+					else
+
+						@R_home_dog_14_or_more.push(m)
+
+
+					end
+
+				end
+
+			elsif m.dog == team
+
+				@all_road_matchups.push(m)
+
+				if m.dog_field == "home"
+
+					if m.spread < 3
+
+						@R_road_fave_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@R_road_fave_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@R_road_fave_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@R_road_fave_under14.push(m)
+
+
+					else
+
+						@R_road_fave_14_or_more.push(m)
+
+
+					end
+
+				
+				else
+
+					if m.spread < 3
+
+						@R_home_fave_under3.push(m)
+
+
+					elsif m.spread.to_f >= 3 || m.spread.to_f < 7
+
+						@R_home_fave_under7.push(m)
+					
+
+					elsif m.spread.to_f >= 7 || m.spread.to_f < 10
+
+						@R_home_fave_under10.push(m)
+					
+
+					elsif m.spread.to_f >= 10 || m.spread.to_f < 14
+
+						@R_home_fave_under14.push(m)
+
+
+					else
+
+						@R_home_fave_14_or_more.push(m)
+
+
+					end
+
+
+				end
+
+			end
+
+		end
+
+	end
+
+	
+	def compute_home_amv
+
+		score_holder = []
+
+		@all_home_matchups.each do |m|
+
+			if m.fav == @matchup.fav
+
+				if m.fav_field == "home"
+
+					num = m.home_score.to_i - m.road_score.to_i
+
+					score_holder.push(num)
+
+
+				else
+
+					num = m.road_score.to_i - m.home_score
+
+					score_holder.push(num)
+
+
+				end
+
+
+			else m.dog == @matchup.fav
+
+				if m.dog_field == "home"
+
+					num = m.home_score.to_i - m.road_score.to_i
+
+					score_holder.push(num)
+
+				else
+
+					num = m.road_score.to_i - m.home_score.to_i
+
+					score_holder.push(num)
+
+
+				end
+
+			end
+
+		end
+
+		@home_amv = score_holder.inject(0){|sum,x| sum + x }
+
+	end
+
+	
+	def compute_road_amv
+
+		score_holder = []
+
+		@all_road_matchups.each do |m|
+
+			if m.fav == @matchup.dog
+
+				if m.fav_field == "home"
+
+					num = m.home_score.to_i - m.road_score.to_i
+
+					score_holder.push(num)
+
+
+				else
+
+					num = m.road_score.to_i - m.home_score
+
+					score_holder.push(num)
+
+
+				end
+
+
+			else m.dog == @matchup.dog
+
+				if m.dog_field == "home"
+
+					num = m.home_score.to_i - m.road_score.to_i
+
+					score_holder.push(num)
+
+				else
+
+					num = m.road_score.to_i - m.home_score.to_i
+
+					score_holder.push(num)
+
+
+				end
+
+			end
+
+		end
+
+		@road_amv = score_holder.inject(0){|sum,x| sum + x }
 
 	end
 end
