4c9ea63cf3da167b500442ab54eae63ba59cbac6
mklarman <mklarman@hotmail.com>
2019-05-10 11:06:56 -0400

    debug form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190509183352) do
+ActiveRecord::Schema.define(version: 20190510150255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20190509183352) do
     t.string "spread_result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "innings"
   end
 
   create_table "selections", force: :cascade do |t|
